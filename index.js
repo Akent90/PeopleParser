48161bf33aa532de9ce07fa5705566e9adde9169
akent <alexanderkent099@gmail.com>
2024-02-12 15:01:28 -0500

    Created addDepartment function to insert new departments into the database

--- index.js
+++ index.js
@@ -87,3 +87,20 @@ async function viewAllEmployees() {
         console.error(err);
     }
 }
+
+async function addDepartment() {
+    const { departmentName } = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'What is the name of the department?',
+        }
+    ]);
+
+    try {
+        await db.query('INSERT INTO department (name) VALUES (?)', [departmentName]);
+        console.log(`Added ${departmentName} to the database`);
+    } catch (err) {
+        console.error(err);
+    }
+}
