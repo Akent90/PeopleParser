097bca76173b63a7854a170160ab69a9c9ebfcae
akent <alexanderkent099@gmail.com>
2024-02-12 14:21:52 -0500

    Implemented main menu using inquirer for various employee management actions

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ const db = mysql.createConection({
     databade: process.env.DB_NAME
 }).promise();
 
+const mainMenu = async () => {
+    try {
+        const { action } = await inquirer.prompt([
+           {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?', 
+            choices: [
+                'View all departments',
+                'View all roles', 
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ],
+           }, 
+        ]);
+    }
+}
