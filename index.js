2819a2bff9c132bd1a7f0452c0ebfe9047033152
akent <alexanderkent099@gmail.com>
2024-02-13 16:28:03 -0500

    Add comments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+// Create the connection to the database 
 const db = mysql.createConection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
