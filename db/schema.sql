e9b444d9fde92459a9973f737eed7793df31332b
akent <alexanderkent099@gmail.com>
2024-02-14 13:11:16 -0500

    Added SQL script to create 'department' table with id and name fields

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS EmplyeeDirectory_db;
 CREATE DATABASE EmplyeeDirectory_db;
 
 USE EmplyeeDirectory_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30) NOT NULL
+);
