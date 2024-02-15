eab881fbb01954a9ec536aa1ca250ba6a3f8fd11
akent <alexanderkent099@gmail.com>
2024-02-15 14:45:30 -0500

    Fixing typos for db connection

--- db/index.js
+++ db/index.js
@@ -3,11 +3,11 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 // Create the connection to the database 
-const db = mysql.createConection({
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS, 
-    databade: process.env.DB_NAME
+    database: process.env.DB_NAME
 }).promise();
 
 module.exports = db;
