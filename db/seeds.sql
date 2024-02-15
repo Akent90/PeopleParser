6ef520d5a1f395a55a97352203060ba00ad89b78
akent <alexanderkent099@gmail.com>
2024-02-15 13:03:50 -0500

    Inserted initial employee records into 'employee' table with role and manager references

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,3 +11,9 @@ INSERT INTO role (title, salary, department_id) VALUES
 ('HR Manager', 70000, 2),
 ('Accountant', 75000, 3),
 ('Marketing Coordinator', 60000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+('John', 'Locke', 1, NULL),
+('Desmond', 'Hume', 2, NULL),
+('Juliet', 'Burke', 3, NULL),
+('Benjamin', 'Linus', 4, 1);
