4d6443c32c05bd9a99691a3eabf7c17ef4a9695a
akent <alexanderkent099@gmail.com>
2024-02-15 12:59:16 -0500

    Added initial role entries into 'role' table with titles, salaries, and department associations

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,8 @@ INSERT INTO department (name) VALUES
 ('Finance'),
 ('Marketing');
 
+INSERT INTO role (title, salary, department_id) VALUES
+('Software Engineer', 80000, 1),
+('HR Manager', 70000, 2),
+('Accountant', 75000, 3),
+('Marketing Coordinator', 60000, 4);
