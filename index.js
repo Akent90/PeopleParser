d1f98e7a7296b0ab5062bec4d517321bd4920fd6
akent <alexanderkent099@gmail.com>
2024-02-16 12:32:45 -0500

    Add comments to index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Importing the mainMenu function from the mainMenu.js file 
 const mainMenu = require('./mainMenu');
 
 mainMenu().catch(err => {
     console.error('Error:', err);
+    // Exiting the app with a non-zero status code indicates that an error occurred 
     process.exit(1);
 });
